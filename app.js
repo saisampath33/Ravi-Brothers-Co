d0f70d2fcc2b51a7a9b0b7dab4d974cc63c367fa
saisampath33 <sampathmay10@gmail.com>
2025-07-08 12:52:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,36 +1,81 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static(__dirname+'/public'));
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname+'/front.html');
+//     // res.render("./public")
+// })
+
+// app.get('/index.html',(req,res)=>{
+//     res.sendFile(__dirname+'/index.html');
+// })
+// app.get('/start.html',(req,res)=>{
+//     res.sendFile(__dirname+'/start.html');
+// })
+
+// app.get("/About.html",(req,res)=>{
+//     res.sendFile(__dirname+'/About.html');
+// })
+
+// app.get('/contactus.html',(req,res)=>{
+//     res.sendFile(__dirname+'/contactus.html');
+// })
+
+// app.get('/login.html',(req,res)=>{
+//     res.sendFile(__dirname+'/login.html');
+// })
+
+
+// app.get('/email.html',(req,res)=>{
+//     res.sendFile(__dirname+'/email.html');
+// })
+
+
+// app.listen(5581);
+
+
+
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/public'));
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/front.html');
-    // res.render("./public")
-})
+// Routes for static HTML pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front.html'));
+});
 
-app.get('/index.html',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
-})
-app.get('/start.html',(req,res)=>{
-    res.sendFile(__dirname+'/start.html');
-})
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.get("/About.html",(req,res)=>{
-    res.sendFile(__dirname+'/About.html');
-})
+app.get('/start.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'start.html'));
+});
 
-app.get('/contactus.html',(req,res)=>{
-    res.sendFile(__dirname+'/contactus.html');
-})
+app.get('/About.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'About.html'));
+});
 
-app.get('/login.html',(req,res)=>{
-    res.sendFile(__dirname+'/login.html');
-})
+app.get('/contactus.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contactus.html'));
+});
 
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
 
-app.get('/email.html',(req,res)=>{
-    res.sendFile(__dirname+'/email.html');
-})
+app.get('/email.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'email.html'));
+});
 
+module.exports = app; // No app.listen() for vercel deployment
 
-app.listen(5581);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
